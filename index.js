81a7e2d48e4da9f1f52a53e8ebfdc94cd599d8fa
Brandon Durham <brandon@smallparade.com>
2022-05-23 09:43:25 -0500

    chore: remove check on pr description

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ const jiraPrefix = core.getInput("jira-prefix");
 async function run() {
   try {
     const prTitle = github.context.payload.pull_request.title;
-    const prBody = github.context.payload.pull_request.body;
 
     let regex = new RegExp(`${jiraPrefix}-[0-9]+`);
-    if (!regex.test(prTitle) || !regex.test(prBody)) {
-      core.setFailed("Jira Issue Key missing in PR title or description.");
+    if (!regex.test(prTitle)) {
+      core.setFailed("Jira Issue Key missing in PR title.");
       return;
     }
   } catch (error) {
